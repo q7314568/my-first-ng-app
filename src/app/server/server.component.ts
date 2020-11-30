3e6c238f0816e7a53c38a04b15d169b4ffa4cf13
q7314568 <q7314568@gmail.com>
2020-12-01 00:33:36 +0800

    Styling Elements Dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,17 +2,26 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html'
 })
-export class ServerComponent{
+
+export class ServerComponent {
   // tslint:disable-next-line: no-inferrable-types
   serverId: number = 10;
   // tslint:disable-next-line: no-inferrable-types
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   // tslint:disable-next-line: typedef
-  getServerStatus(){
+  getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
